0e9a601233beb33913a4a2d35a04f36588ce989f
hamza-aloglu <hamza-aloglu@hotmail.com>
2022-08-17 10:08:56 +0300

    register page not closing bug fixed.

--- views/services/dynamicRegister-Login.php
+++ views/services/dynamicRegister-Login.php
@@ -1,36 +1,37 @@
 <script>
-const loginContainer = document.getElementById('login_view');
-const textArea = document.getElementById('text');
-const registerButton = document.getElementById('register_button');
+    const loginContainer = document.getElementById('login_view');
+    const textArea = document.getElementById('text');
+    const registerButton = document.getElementById('register_button');
 
-const registerContainer = document.getElementById('register_view');
-let isLoginPageOpen = false;
+    const registerContainer = document.getElementById('register_view');
+    let isLoginPageOpen = false;
+    let isRegisterPageOpen = false;
 
-<?php if (isset($isEmailValid)):
+    <?php if (isset($isEmailValid)):
     if (!$isEmailValid):
-        ?>
-        registerContainer.classList.remove('d-none');
+    ?>
+    isRegisterPageOpen = true;
+    registerContainer.classList.remove('d-none');
     <?php
     else: ?>
-        isLoginPageOpen = true;
-        loginContainer.classList.remove('d-none');
+    isLoginPageOpen = true;
+    loginContainer.classList.remove('d-none');
 
     <?php endif; endif; ?>
 
-let isRegisterPageOpen = false;
 
 
-textArea.addEventListener('click', function () {
-if (isLoginPageOpen || isRegisterPageOpen) {
-loginContainer.classList.add('d-none');
-registerContainer.classList.add('d-none');
-isLoginPageOpen = false;
-isRegisterPageOpen = false;
-}
-});
+    registerButton.addEventListener('click', function () {
+        registerContainer.classList.remove('d-none');
+        isRegisterPageOpen = true;
+    });
 
-registerButton.addEventListener('click', function () {
-registerContainer.classList.remove('d-none');
-isRegisterPageOpen = true;
-});
+    textArea.addEventListener('click', function () {
+        if (isLoginPageOpen || isRegisterPageOpen) {
+            loginContainer.classList.add('d-none');
+            registerContainer.classList.add('d-none');
+            isLoginPageOpen = false;
+            isRegisterPageOpen = false;
+        }
+    });
 </script>
